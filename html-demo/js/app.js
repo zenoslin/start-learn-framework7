2f0308d81872ec624ea419d0311d407e6c9ef0b0
zenoslin <zenoslin@outlook.com>
2019-09-24 17:20:36 +0800

    feat: 增加framework示例

---
+++ html-demo/js/app.js
@@ -0,0 +1,11 @@
+// Dom7
+var $ = Dom7;
+
+var app = new Framework7({
+  id: "io.framework7.testapp",
+  root: "#app",
+  routes: routes,
+  vi: {
+    placementId: "pltd4o7ibb9rc653x14"
+  }
+});
---
+++ html-demo/js/routes.js
@@ -0,0 +1,14 @@
+var routes = [
+  // Index page
+  {
+    path: "/",
+    url: "./index.html",
+    name: "home"
+  },
+  // About page
+  {
+    path: "/about/",
+    url: "./pages/about.html",
+    name: "about"
+  }
+];
